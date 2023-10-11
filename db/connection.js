1a079094b485a1d2c981ec8031c810e42d63c1cd
Arwa Sharif <arwas@google.com>
2023-10-11 13:20:15 -0500

    initial commit with connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,13 @@
+const { Sequelize, DataTypes, Models } = require("sequelize");
+const path = require("path");
 
-
-
-
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sequelize"),
+});
 
 module.exports = {
-    db,
-    
-}
+  db,
+  DataTypes,
+  Models,
+};
